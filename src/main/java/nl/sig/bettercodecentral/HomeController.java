2a5359f29c4644789414368216591be23e424a6b
Rob van der Leek <robvanderleek@gmail.com>
2017-02-20 14:55:55 +0100

    Update HomeController.java

--- src/main/java/nl/sig/bettercodecentral/HomeController.java
+++ src/main/java/nl/sig/bettercodecentral/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Welcome to Better Code Central!";
+        return "Warm welcome to Better Code Central!";
     }
 
 }
