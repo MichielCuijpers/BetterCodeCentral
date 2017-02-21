43bdd0be61ca1652a246d2f96499b65550348b79
mcadariu <cadariu.mircea@gmail.com>
2017-02-21 22:12:02 +0100

    Update HomeControllerTest.java

--- src/test/java/nl/sig/bettercodecentral/HomeControllerTest.java
+++ src/test/java/nl/sig/bettercodecentral/HomeControllerTest.java
@@ -26,7 +26,7 @@ public class HomeControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(content().string(equalTo("Hello there! Welcome to Better Code Central!")));
+            .andExpect(content().string(equalTo("Hello there! Warm Welcome to Better Code Central!")));
     }
 
 }
