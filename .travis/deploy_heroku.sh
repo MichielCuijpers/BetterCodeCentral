31723f8fdbd2dfc59b1480ba43952f3aea970435
Rob van der Leek <r.vanderleek@sig.eu>
2017-02-21 20:36:40 +0100

    Make Heroku app explicit for now

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
-docker login --email=_ --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com
-heroku container:push web
+docker login -e _ -u _ --password=$HEROKU_AUTH_TOKEN registry.heroku.com
+heroku container:push web --app lit-stream-81480
